e76de0bfd739a625b6f43a2d28b31bddacf93f4a
graczek <raczek.grz@gmail.com>
2019-10-26 15:14:02 +0200

    init

---
+++ src/com/graczek/tictactoe/Main.java
@@ -0,0 +1,19 @@
+package com.graczek.tictactoe;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
