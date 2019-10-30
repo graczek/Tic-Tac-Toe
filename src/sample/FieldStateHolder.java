ddf1c76bf3c960c3ba8e02f1ce5fc1df2d1d2e39
graczek <raczek.grz@gmail.com>
2019-10-30 16:32:11 +0100

    added fieldstateholder obj

---
+++ src/sample/FieldStateHolder.java
@@ -0,0 +1,20 @@
+package sample;
+
+import sample.enums.FieldState;
+
+public class FieldStateHolder {
+
+    private FieldState fieldState = FieldState.X;
+
+    public FieldState getCurrentFieldStateAndSetNext() {
+        FieldState fieldStateToReturn = fieldState;
+        fieldState = this.fieldState == FieldState.X ? FieldState.O : FieldState.X;
+        return fieldStateToReturn;
+    }
+
+    public void resetFieldToInitialState() {
+        fieldState = FieldState.X;
+    }
+
+
+}
